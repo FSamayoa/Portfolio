51efdc375d1d9208f2c44a65cecfabdd0473ff4b
Samayoa <fsamayoal@gmail.com>
2023-12-19 16:54:50 -0600

    save

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Home from '../src/components/home'
+import Footer from '../src/components/footer'
+
+function App() {
+  return (
+
+      <div>
+        <Home/>
+        <Footer/>
+      </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/footer.jsx
@@ -0,0 +1,43 @@
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/fernando-samayoa-01116964/"
+          target="blank"
+        >
+          <img
+            align="center"
+            src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"
+            alt="logo linkedin"
+            height="40"
+            width="40"
+          />
+        </a>
+        <a href="mailto:fsamayoal@gmail.com" target="blank">
+          <img
+            align="center"
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg"
+            alt=" logo Gmail"
+            height="40"
+            width="40"
+          />
+        </a>
+        <a href="https://github.com/FSamayoa" target="blank">
+          <img
+            align="center"
+            src="../src/assets/github-mark-white.svg"
+            alt="logo github"
+            height="40"
+            width="40"
+          />
+        </a>
+      </div>
+      <div>
+        <button>Download CV</button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
