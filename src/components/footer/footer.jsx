d327dca62a7c313923ad7e42a7ffe6be893e6a84
Samayoa <fsamayoal@gmail.com>
2023-12-28 20:25:51 -0600

    logos, imagenes

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
         <a href="https://github.com/FSamayoa" target="blank">
           <img
             align="center"
-            src="../src/assets/github-mark-white.svg"
+            src="https://i.imgur.com/dy2uNAS.png"
             alt="logo github"
             height="40"
             width="40"
--- src/components/projec/ProjectsBase.jsx
+++ src/components/projec/ProjectsBase.jsx
@@ -1,7 +1,7 @@
 export const projectsData = [
 
     {
-      imagenSrc: '../src/assets/p1.png',
+      imagenSrc: 'https://i.imgur.com/1b6MC3O.png',
       tituloProy:'BFS Envios',
       descripcion: 'Este proyecto culminante del bootcamp integra todas las herramientas adquiridas, así como tabien nuevas, para dar vida a una aplicación completa; De una empresa de mensajería con funcionalidades, como contratación de pedidos, seguimiento en tiempo real, autenticación de usuarios, descarga de facturas y notificaciones por correo electrónico. Además de un panel de administración para la gestion de usuarios y envios.',
       repoLink: 'https://github.com/GonzalMartin13/BFS-PFHenry',
@@ -9,7 +9,7 @@ export const projectsData = [
       id:1
     },
     {
-      imagenSrc: '../src/assets/p2.png',
+      imagenSrc: 'https://i.imgur.com/Nj8Uc5Z.png',
       tituloProy:'Countries',
       descripcion: 'Desarrollado como proyecto individual para una evaluación avanzada del bootcamp, Countries es una aplicación fullstack que facilita la búsqueda de información sobre países. Permite conocer las actividades que se pueden realizar en cada país y proporciona datos adicionales mediante la integración de una API externa.',
       repoLink: 'https://github.com/FSamayoa/proyectCountries',
@@ -17,7 +17,7 @@ export const projectsData = [
       id:2
     },
     {
-      imagenSrc: '../src/assets/p3.png',
+      imagenSrc: 'https://i.imgur.com/qYwsP7E.png',
       tituloProy:'Consulta Clima',
       descripcion: 'Proyecto personal con un enfoque en la búsqueda de películas. La aplicación, conectada a una API, brinda detalles esenciales como póster, año de lanzamiento, elenco y más. Su interfaz permite a los usuarios obtener información completa sobre sus películas favoritas.',
       repoLink: 'https://github.com/FSamayoa/aplicacionDelClima',
@@ -25,7 +25,7 @@ export const projectsData = [
       id:3
     },
     {
-      imagenSrc: '../src/assets/p4.png',
+      imagenSrc: 'https://i.imgur.com/IOyMYna.png',
       tituloProy:'Busqueda de peliculas',
       descripcion: 'Proyecto personal sencillo, pero con una gran funcionalidad de buscar una pelicula y poder obtener informacion de ella, tanto el poster, año, actores, etc, aplicacion conectada a una api para poder obtener todos estos datos.',
       repoLink: 'https://github.com/FSamayoa/proyecto-api-peliculas',
