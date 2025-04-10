e43273514b113829dc0f2df830c2f0c943c2a198
Samayoa <fsamayoal@gmail.com>
2025-04-10 12:42:53 -0600

    actualizacion con los videos

--- src/components/projec/ProjectsBase.jsx
+++ src/components/projec/ProjectsBase.jsx
@@ -1,12 +1,21 @@
 export const projectsData = [
 
+
+    {
+      imagenSrc: 'https://i.imgur.com/UX28H6u.png',
+      tituloProy:'FBA NAcional',
+      descripcion: 'Proyecto freelance para generar contenido de football americano, Edicion de videos para youtube, instagram y tiktok; ademas de colaborar con la base de datos de la aplicacion.',
+      // repoLink: 'https://github.com/FSamayoa/proyecto-api-peliculas',
+      proyectoLink:'https://www.youtube.com/@FBA-Nacional',
+      id:1
+    },
     {
       imagenSrc: 'https://i.imgur.com/1b6MC3O.png',
       tituloProy:'BFS Envios',
       descripcion: 'Este proyecto culminante del bootcamp integra todas las herramientas adquiridas, así como tabien nuevas, para dar vida a una aplicación completa; De una empresa de mensajería con funcionalidades, como contratación de pedidos, seguimiento en tiempo real, autenticación de usuarios, descarga de facturas y notificaciones por correo electrónico. Además de un panel de administración para la gestion de usuarios y envios.',
       repoLink: 'https://github.com/GonzalMartin13/BFS-PFHenry',
       proyectoLink:'https://bfsonline.vercel.app/',
-      id:1
+      id:2
     },
     {
       imagenSrc: 'https://i.imgur.com/Nj8Uc5Z.png',
@@ -14,7 +23,7 @@ export const projectsData = [
       descripcion: 'Desarrollado como proyecto individual para una evaluación avanzada del bootcamp, Countries es una aplicación fullstack que facilita la búsqueda de información sobre países. Permite conocer las actividades que se pueden realizar en cada país y proporciona datos adicionales mediante la integración de una API externa.',
       repoLink: 'https://github.com/FSamayoa/proyectCountries',
       // proyectoLink:'',
-      id:2
+      id:3
     },
     {
       imagenSrc: 'https://i.imgur.com/qYwsP7E.png',
@@ -22,7 +31,7 @@ export const projectsData = [
       descripcion: 'Proyecto personal con un enfoque en la búsqueda de películas. La aplicación, conectada a una API, brinda detalles esenciales como póster, año de lanzamiento, elenco y más. Su interfaz permite a los usuarios obtener información completa sobre sus películas favoritas.',
       repoLink: 'https://github.com/FSamayoa/aplicacionDelClima',
       proyectoLink:'https://app-de-consulta-del-clima.netlify.app/',
-      id:3
+      id:4
     },
     {
       imagenSrc: 'https://i.imgur.com/BRN5ESo.png',
@@ -30,16 +39,17 @@ export const projectsData = [
       descripcion: 'Aplicación móvil como herramienta para consultar resultados en tiempo real, explorar el calendario de juegos y verificar las posiciones de los equipos. La aplicación cuenta con una interfaz intuitiva, notificaciones instantáneas para resultados, anuncios y conexión a tablas externas para mostrar información.',
       // repoLink: '',
       proyectoLink:'http://bit.ly/3Ppyf43',
-      id:4
+      id:5
     },
-    // {
-    //   imagenSrc: 'https://i.imgur.com/IOyMYna.png',
-    //   tituloProy:'Busqueda de peliculas',
-    //   descripcion: 'Proyecto personal sencillo, pero con una gran funcionalidad de buscar una pelicula y poder obtener informacion de ella, tanto el poster, año, actores, etc, aplicacion conectada a una api para poder obtener todos estos datos.',
-    //   repoLink: 'https://github.com/FSamayoa/proyecto-api-peliculas',
-    //   proyectoLink:'https://busqueda-de-peliculas-por-api.netlify.app/',
-    //   id:5
-    // },
+    {
+      imagenSrc: 'https://i.imgur.com/IOyMYna.png',
+      tituloProy:'Busqueda de peliculas',
+      descripcion: 'Proyecto personal sencillo, pero con una gran funcionalidad de buscar una pelicula y poder obtener informacion de ella, tanto el poster, año, actores, etc, aplicacion conectada a una api para poder obtener todos estos datos.',
+      repoLink: 'https://github.com/FSamayoa/proyecto-api-peliculas',
+      proyectoLink:'https://busqueda-de-peliculas-por-api.netlify.app/',
+      id:6
+    },
+   
 
   ];
 
